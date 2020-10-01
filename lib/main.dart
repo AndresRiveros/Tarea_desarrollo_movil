d9fee86796645fee06f2a43d93a24c4a0a593ecc
AndresRiveros <andres.riveros@sansano.usm.cl>
2020-10-01 16:45:31 -0300

    Tarea_desarrollo_movil

---
+++ lib/main.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.purple[200],
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+
+                //////////     IMG    /////////
+                backgroundImage: AssetImage('images/chayanne.jpg'),
+              ),
+
+              //////////     NOMBRE    /////////
+              Text(
+                'Chayanne',
+                style: TextStyle(
+                  fontSize: 40.0,
+                  fontFamily: 'Kalam',
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+
+              //////////     OCUPACIÓN    /////////
+              Text(
+                'Cantante',
+                style: TextStyle(
+                  fontSize: 28.0,
+                  fontFamily: 'Kalam',
+                  color: Colors.white,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.call,
+                    color: Colors.red,
+                  ),
+
+                  //////////     N° DE TELÉFONO   /////////
+                  title: Text(
+                    '+56 9 12345678',
+                    style: TextStyle(
+                      color: Colors.red,
+                      fontSize: 18.0,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.red,
+                  ),
+
+                  //////////     CORREO   /////////
+                  title: Text(
+                    'este.noesunmail@outlook.com',
+                    style: TextStyle(
+                      color: Colors.red,
+                      fontSize: 18.0,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                child: ListTile(
+                  leading: Text(
+                    '@',
+                    style: TextStyle(
+                      color: Colors.red,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+
+                  //////////     @    /////////
+                  title: Text(
+                    'chayanne',
+                    style: TextStyle(
+                      color: Colors.red,
+                      fontSize: 18.0,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.directions,
+                    color: Colors.red,
+                  ),
+
+                  //////////     DIRECCIÓN   /////////
+                  title: Text(
+                    'Tomás Andrews 25, Providencia',
+                    style: TextStyle(
+                      color: Colors.red,
+                      fontSize: 18.0,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
